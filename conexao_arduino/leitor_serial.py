eda9a9b819f868d7a2a9c99acf9733878e9282f7
laiscamprin <camprinbiaelais@gmail.com>
2025-10-01 23:04:03 -0300

    alteração leitor_serial.py
    alterando para receber as informações corretas do arduino

--- conexao_arduino/leitor_serial.py
+++ conexao_arduino/leitor_serial.py
@@ -1,35 +1,25 @@
-import serial
-import mysql.connector
-from datetime import datetime
-
-PORTA_SERIAL = 'COM3'  # Altere conforme seu sistema
-BAUD_RATE = 9600 
-
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '****',
-    'database': 'exemplo_serial'
-}
-
-# Conecta ao banco
-conexao = mysql.connector.connect(**DB_CONFIG)
-cursor = conexao.cursor()
-
+#)
+#""")
 # Conecta à serial
 arduino = serial.Serial(PORTA_SERIAL, BAUD_RATE, timeout=2)
-
 try:
     while True:
         linha = arduino.readline().decode('utf-8').strip()
         if linha:
             partes = linha.split(';')
-            data_obj = datetime.strptime(partes[0], "%Y-%m-%d").date()
-            hora_obj = datetime.strptime(partes[1], "%H:%M:%S").time()
-            info = partes[2]
-
-            cursor.execute("INSERT INTO registros (data, hora, informacao) VALUES (%s, %s, %s)",
-                           (data_obj, hora_obj, info))
+            data_obj = date.today()
+            hora_obj = datetime.now().time()
+            hora_str = hora_obj.strftime("%H:%M:%S")
+            temperatura = partes[0]
+            umidade_ar = partes[1]
+            umidade_solo = partes[2]
+            irrigacao = partes[3].strip().lower()
+            if irrigacao not in ['true', 'false']:
+                irrigacao = None  # ou 'false', dependendo do que faz sentido
+            cursor.execute(
+                "INSERT INTO registros (data, hora, temperatura, umidade_ar, umidade_solo, irrigacao) VALUES (%s, %s, %s, %s, %s, %s)",
+                (data_obj, hora_str, temperatura, umidade_ar, umidade_solo, irrigacao)
+            )
             conexao.commit()
             print("Registro inserido:", linha)
 except KeyboardInterrupt:
@@ -38,4 +28,3 @@ finally:
     cursor.close()
     conexao.close()
     arduino.close()
-    
